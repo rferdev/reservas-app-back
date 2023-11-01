a08b18e2606f88303934e9d2a207d3a0d9152763
rferdev <rferreira.dev@hotmail.com>
2023-11-01 17:28:50 -0500

    ORDER BY salida DESC

--- models/vuelo.js
+++ models/vuelo.js
@@ -2,7 +2,7 @@ import { pool } from '../db/config.js';
 
 class Vuelo {
   static async getAllVuelos() {
-    const query = 'SELECT * FROM vuelos ORDER BY salida';
+    const query = 'SELECT * FROM vuelos ORDER BY salida DESC';
     const result = await pool.query(query);
     return result.rows;
   }
